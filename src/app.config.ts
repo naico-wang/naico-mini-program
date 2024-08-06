f582f6dd44cf20a3aa6333a35960ec8693f06e7e
Naico Wang <>
2024-08-06 16:26:19 +0800

    feat(tabbar): add tabbar

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,30 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/about/index'
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
-  }
+  },
+  tabBar: {
+    color: '#222222',
+    selectedColor: '#222222',
+    backgroundColor: '#ffffff',
+    borderStyle: 'black',
+    list: [{
+      text: '博客',
+      pagePath: 'pages/index/index',
+      iconPath: 'images/icon-home.png',
+      selectedIconPath: 'images/icon-home-selected.png'
+    }, {
+      text: '关于我',
+      pagePath: 'pages/about/index',
+      iconPath: 'images/icon-me.png',
+      selectedIconPath: 'images/icon-me-selected.png'
+    }]
+  },
+  lazyCodeLoading: 'requiredComponents'
 })
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,21 +1,9 @@
 import { View } from '@tarojs/components'
-import { useState } from 'react'
-
-import EnContent from '@/components/en'
-import CnContent from '@/components/cn'
 
 import './index.less'
 
 export default function Index () {
-  const [ showEnVersion, setShowEnVersion ] = useState<boolean>(false)
-  const onButtonClick = () => {
-    setShowEnVersion(!showEnVersion)
-  }
   return (
-    <View className='page-container'>
-      <View className='float-button' onClick={onButtonClick}>{ showEnVersion ? '中文' : '英文'}</View>
-      { showEnVersion ? <EnContent /> : <CnContent /> }
-      <EnContent />
-    </View>
+    <View className='logo' />
   )
 }
